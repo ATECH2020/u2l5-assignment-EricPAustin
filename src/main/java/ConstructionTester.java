46357a39c663ad598ad5c9d7148d378d31ee2609
Eric Austin <eric.237969@nv.ccsd.net>
2020-09-28 01:27:52 +0000

    Finished ConstructionTest and HowFarAway classes.

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,27 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+        //Attributes
+        Scanner in = new Scanner(System.in);
+        double lumberCost = 8.0;
+        double windowCost = 11.0;
+        double taxRate;
+        int boards, windows;
+        double total, grandTotal;
+
+        //Taking input
+        System.out.println("Enter the sales tax rate: ");
+        taxRate = in.nextDouble();
+        System.out.println("How many boards do you need?");
+        boards = in.nextInt();
+        System.out.println("How many windows do you need?");
+        windows = in.nextInt();
+
+        // Printing the outputs.
+        Construction myC = new Construction(8, 11, taxRate);
+        total = myC.lumberCost(boards) + myC.windowCost(windows);
+        System.out.println("Total: " + total);
+        grandTotal = myC.grandTotal(total);
+        System.out.print("Grand Total: " + grandTotal);
     }
 }
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,23 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+        //Attributes
+        Scanner in = new Scanner(System.in);
+        double firstLat, firstLon, secondLat, secondLon;
+
+        //Input from user and object creation
+        System.out.print("Enter the latitude of the starting location: ");
+        firstLat = in.nextDouble();
+        System.out.print("Enter the longitude of the starting location: ");
+        firstLon = in.nextDouble();
+        System.out.print("Enter the latitude of the ending location: ");
+        secondLat = in.nextDouble();
+        System.out.print("Enter the longitude of the ending location: ");
+        secondLon = in.nextDouble();
+        GeoLocation g1 = new GeoLocation(firstLat, firstLon);
+        GeoLocation g2 = new GeoLocation(secondLat, secondLon);
+
+        //Printing out the distanceS
+        System.out.print("The distance is " + g1.distanceFrom(g2) + " miles.");
     }
 }
